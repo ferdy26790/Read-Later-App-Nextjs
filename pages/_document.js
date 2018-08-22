238417a8eddfd19535a72c80643c42afe2d11f04
ferdy26790 <ferdy26790@gmail.com>
2018-08-22 09:07:45 +0700

    initial setup and design template

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+ 
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="/_next/static/style.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+        </body>
+      </html>
+    )
+  }
+}
---
+++ pages/pocket.js
@@ -0,0 +1,31 @@
+import '../scss/style.scss'
+export default () => (
+  <div className="container-pocket">
+    <ul className="collection row">
+
+      <div className="col m3">
+        <li className="collection-item avatar">
+          <img src="https://usatftw.files.wordpress.com/2014/06/usp-nba_-los-angeles-lakers-at-sacramento-kings.jpg?w=1000&h=600&crop=1" alt="" className="circle"/>
+          <span className="title">Title</span>
+          <p>First Line 
+            Second Line
+          </p>
+          <a href="#!"><i className="material-icons">Continue</i></a>
+        </li>
+      </div>
+
+      <div className="col m3">
+        <li className="collection-item avatar">
+        <img src="https://usatftw.files.wordpress.com/2014/06/usp-nba_-los-angeles-lakers-at-sacramento-kings.jpg?w=1000&h=600&crop=1" alt="" className="circle"/>
+          <span className="title">Title</span>
+          <p>First Line 
+            Second Line
+          </p>
+          <a href="#!"><i className="material-icons">Continue</i></a>
+        </li>
+      </div>
+      
+      
+    </ul>
+  </div>
+)
